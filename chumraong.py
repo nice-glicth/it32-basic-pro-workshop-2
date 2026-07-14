4fda39faf7db0a5980801e34e7c71901b009017d
bigza55a <bookmararam@gmail.com>
2026-07-14 16:30:51 +0700

    Write program in class01

---
+++ chumraong.py
@@ -0,0 +1,12 @@
+# Gun = "1000"
+# Cost_price = "10000"
+quality = int(input("จำนวนปืน ") )
+
+
+cost_price = int(input(quality * 10000))
+
+sell_price = int(input(quality * 12000))
+
+print("Total" "cost_price" "sell_price")
+
+
